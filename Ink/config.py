47c5c108c46b5f0f10de5d68a287752acd148597
Yabel <creeperlani@gmail.com>
2020-12-11 14:48:20 -0400

    Finished redoing the navbar and the css code. Fixed bugs.

--- Ink/config.py
+++ Ink/config.py
@@ -1,21 +1,3 @@
-from flask_session import Session
-from tempfile import mkdtemp
-from Ink.__init__ import *
-
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
-
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+version https://git-lfs.github.com/spec/v1
+oid sha256:02078e297aad38fcdab044454c94e1ed80f62468d14df23a3928822cbb69fb49
+size 574
